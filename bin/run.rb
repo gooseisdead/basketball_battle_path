7491b6f351efba168c9c3127112f451c81907247
Victorsloop <torres.victor136@gmail.com>
2020-09-30 14:41:26 -0400

    Hello again

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,8 @@ require_relative '../config/environment'
 ## We will test our cli or classes here , once its built along witht rest 
 
 
-
+### testing git push and pull and commits 
 
 puts "HELLO WORLD"
+
+##hello brodie !
